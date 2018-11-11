6cefeab04ab07a21b42d5998fdcf08c1c20cb3ee
David Feinzimer <dfeinzimer@gmail.com>
2018-11-11 15:33:57 -0800

    11.11.18.5

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-#Version 11.11.18.4
+# Version 11.11.18.5
 # Adapted from http://wang.ecs.fullerton.edu/cpsc485/editdist.html
 # David Feinzimer 4168
 # Requirements
 #    Python 3.x
 # Instructions
+#    No compilation necessary
 #    In project directory run python3 main.py
 
-from array import *
-
-edit_distance = None
 matrix = []
-word_one = None
-word_two = None
+
+
+# Finds the optimal global alignment
+def find_alignment():
+    b = []  # Word one alignment
+    c = []  # Word two alignment
+    row = len(word_one)
+    column = len(word_two)
+    while len(b) != len(word_one):
+        if word_one[row - 1] == word_two[column - 1]:
+            b.insert(0, word_one[row - 1])
+            c.insert(0, word_two[column - 1])
+            column -= 1
+            row -= 1
+        elif matrix[row][column] - 1 == matrix[row - 1][column - 1]:
+            b.insert(0, word_one[row - 1])
+            c.insert(0, word_two[column - 1])
+            column -= 1
+            row -= 1
+        elif matrix[row][column] - 1 == matrix[row][column - 1]:
+            b.insert(0, '_')
+            c.insert(0, word_two[column - 1])
+            column -= 1
+        elif matrix[row][column] - 1 == matrix[row - 1][column]:
+            b.insert(0, word_one[row - 1])
+            c.insert(0, '_')
+            row -= 1
+    set_result(matrix[len(word_one)][len(word_two)], b, c)
+
+
+# Fills the matrix with correct values
+def fill_matrix():
+    for row_counter in range(len(word_one)):
+        for column_counter in range(len(word_two)):
+            if word_one[row_counter] == word_two[column_counter]:
+                matrix[row_counter + 1][column_counter + 1] = matrix[row_counter][column_counter]
+            else:
+                matrix[row_counter + 1][column_counter + 1] = min(matrix[row_counter + 1][column_counter],
+                                                                  matrix[row_counter][column_counter],
+                                                                  matrix[row_counter][column_counter + 1]) + 1
+
 
 # Builds an empty matrix with the correct number of rows and columns
 def initialize_matrix():
@@ -24,34 +61,37 @@ def initialize_matrix():
         for j in range(len(word_two)):
             matrix[i+1].append(0)
 
+
 # Iterates through and prints matrix
 def print_matrix():
-    print('    ',end='')
+    print("The matrix:")
+    print('    ', end='')
     for i in range(len(word_two)):
-        print(word_two[i],end='  ')
+        print(word_two[i], end='  ')
     print()
-    row = 0
+    row_num = 0
     for r in matrix:
-        if row == 0:
+        if row_num == 0:
             print(' ', end='')
         else:
-            print(word_one[row-1], end='')
-        row += 1
-        for c in r:
-            print(c, end='  ')
+            print(word_one[row_num-1], end='')
+        row_num += 1
+        for col in r:
+            print(col, end='  ')
         print()  # Newline
         print()  # Spacing
 
+
 # Print results out
-def set_result(a, b, c):
+def set_result(a, arg_b, arg_c):
     print("The edit distance is: " + str(a))
     print()
     print("Alignment is:")
-    for i in range(len(b)):
-        print(b[i],end='')
+    for i in range(len(arg_b)):
+        print(arg_b[i], end='')
     print()
-    for i in range(len(c)):
-        print(c[i],end='')
+    for i in range(len(arg_c)):
+        print(arg_c[i], end='')
     print()
 
 
@@ -64,50 +104,10 @@ word_one = input("The first word: ")
 word_two = input("The second word: ")
 print()
 
-# check_identical()
-
 initialize_matrix()
 
-for row in range(len(word_one)):
-    for column in range(len(word_two)):
-        #print("Comparing " + word_one[row] + " and " + word_two[column])
-        if word_one[row] == word_two[column]:
-            #print("Match found")
-            matrix[row+1][column+1] = matrix[row][column]
-            #print_matrix()
-        else:
-            #print("Match not found")
-            #print("Using " + str(min(matrix[row+1][column], matrix[row][column], matrix[row][column+1])) + " +1 for new entry")
-            matrix[row+1][column+1] = min(matrix[row+1][column], matrix[row][column], matrix[row][column+1]) + 1
-            #print_matrix()
+fill_matrix()
 
-print("The matrix:")
 print_matrix()
 
-b = []  # Word one alignment
-c = []  # Word two alignment
-
-row = len(word_one)
-column = len(word_two)
-
-while len(b) != len(word_one):
-    if word_one[row-1] == word_two[column-1]:
-        b.insert(0,word_one[row-1])
-        c.insert(0,word_two[column-1])
-        column -= 1
-        row -= 1
-    elif matrix[row][column] -1 == matrix[row-1][column-1]:
-        b.insert(0,word_one[row-1])
-        c.insert(0,word_two[column-1])
-        column -= 1
-        row -= 1
-    elif matrix[row][column] -1 == matrix[row][column-1]:
-        b.insert(0,'_')
-        c.insert(0,word_two[column-1])
-        column -= 1
-    elif matrix[row][column] -1 == matrix[row-1][column]:
-        b.insert(0,word_one[row-1])
-        c.insert(0,'_')
-        row -= 1
-
-set_result(matrix[len(word_one)][len(word_two)],b,c)
+find_alignment()
